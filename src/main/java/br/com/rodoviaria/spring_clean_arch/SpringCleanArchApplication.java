372648ab86eded5603e12c4f9d9ec2746576bf2e
CassiusCardoso <cassiuscardosoo@gmail.com>
2025-06-29 12:33:35 -0300

    Clean Arch: Firts steps

---
+++ src/main/java/br/com/rodoviaria/spring_clean_arch/SpringCleanArchApplication.java
@@ -0,0 +1,13 @@
+package br.com.rodoviaria.spring_clean_arch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCleanArchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCleanArchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rodoviaria/spring_clean_arch/SpringCleanArchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rodoviaria.spring_clean_arch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCleanArchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
