b750e9c642d0970a19aa6df4861f57f8c1ed14fb
CassiusCardoso <cassiuscardosoo@gmail.com>
2025-06-30 09:53:41 -0300

    Add: domain/repositories - adicionados

---
+++ src/main/java/br/com/rodoviaria/spring_clean_arch/domain/repositories/PassageiroRepository.java
@@ -0,0 +1,27 @@
+package br.com.rodoviaria.spring_clean_arch.domain.repositories;
+
+import br.com.rodoviaria.spring_clean_arch.domain.entities.Passageiro;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface PassageiroRepository {
+
+    // Salvar passageiro
+    Passageiro salvar(Passageiro passageiro);
+
+    // Buscar passageiro por id;
+    Optional<Passageiro> buscarPassageiroPorId(UUID id);
+
+    // Listar todos os passageiros cadastrados
+    List<Passageiro> listarPassageiros();
+
+    // Buscar por CPF
+    // Suggest: Gemini 30/06 - 09:46
+    Optional<Passageiro> buscarPorCpf(String cpf);
+
+    // Buscar por email
+    // Suggest: Gemini 30/06 - 09:46
+    Optional<Passageiro> buscarPorEmail(String email);
+}
