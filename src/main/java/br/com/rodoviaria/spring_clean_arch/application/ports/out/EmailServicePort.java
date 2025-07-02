596dab2abcb91801f232459984a019bcb16e8a76
CassiusCardoso <cassiuscardosoo@gmail.com>
2025-07-02 11:21:39 -0300

    (add):interface EmailServicePort adicionado

---
+++ src/main/java/br/com/rodoviaria/spring_clean_arch/application/ports/out/EmailServicePort.java
@@ -0,0 +1,6 @@
+package br.com.rodoviaria.spring_clean_arch.application.ports.out;
+
+// Este é o "contrato" que o Caso de Uso irá conhecer.
+public interface EmailServicePort {
+    void enviarEmail(String destinatario, String assunto, String corpo);
+}
