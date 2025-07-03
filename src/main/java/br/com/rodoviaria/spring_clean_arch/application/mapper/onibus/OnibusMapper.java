aeefa5718b93778332af4b29ae14ebe9267971c1
CassiusCardoso <cassiuscardosoo@gmail.com>
2025-07-03 15:28:31 -0300

    (fix): lógica do OnibusMapper alterada

--- src/main/java/br/com/rodoviaria/spring_clean_arch/application/mapper/onibus/OnibusMapper.java
+++ src/main/java/br/com/rodoviaria/spring_clean_arch/application/mapper/onibus/OnibusMapper.java
@@ -3,11 +3,15 @@ package br.com.rodoviaria.spring_clean_arch.application.mapper.onibus;
 import br.com.rodoviaria.spring_clean_arch.application.dto.response.onibus.OnibusResponse;
 import br.com.rodoviaria.spring_clean_arch.domain.entities.Onibus;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface OnibusMapper {
     OnibusMapper INSTANCE = Mappers.getMapper(OnibusMapper.class);
-    OnibusResponse toResponse(Onibus onibus);
 
+    // Diz ao MapStruct: para preencher o campo 'placa' do DTO,
+    // pegue o objeto 'placa' da entidade e acesse seu atributo 'valor'.
+    @Mapping(source = "placa.valor", target = "placa")
+    OnibusResponse toResponse(Onibus onibus);
 }
