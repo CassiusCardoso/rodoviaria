633541664bbcfb13e6a4eba24fbef07b56202b57
CassiusCardoso <cassiuscardosoo@gmail.com>
2025-07-03 11:49:09 -0300

    (fix): value object de Senha alterado

--- src/main/java/br/com/rodoviaria/spring_clean_arch/domain/valueobjects/Senha.java
+++ src/main/java/br/com/rodoviaria/spring_clean_arch/domain/valueobjects/Senha.java
@@ -1,17 +1,17 @@
 package br.com.rodoviaria.spring_clean_arch.domain.valueobjects;
 
 public final class Senha {
-    private final String senhaBruta;
+    private final String senhaHash;
     private static final String REGEX = "^(?=.*[!@#$%^&*()_+=\\-{}\\[\\]:;\"'<>,.?/])(?=.{6,}).*$";
 
     public Senha(String senha) {
         if (senha == null || !senha.matches(REGEX)) {
             throw new IllegalArgumentException("Senha inválida. Mínimo 6 caracteres e pelo menos 1 caractere especial.");
         }
-        this.senhaBruta = senha;
+        this.senhaHash = senha;
     }
 
-    public String getSenhaBruta() {
-        return senhaBruta;
+    public String getSenhaHash() {
+        return senhaHash;
     }
 }
