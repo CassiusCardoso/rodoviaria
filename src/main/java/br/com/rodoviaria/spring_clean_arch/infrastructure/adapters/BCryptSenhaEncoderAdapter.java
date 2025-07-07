068c678ea83a9ecf3884c541ac4543882da8ddaa
CassiusCardoso <cassiuscardosoo@gmail.com>
2025-07-07 14:24:04 -0300

    (Feature):e-impl): Serviços externosra codificar a senha e criar um token JWT de validação criados

--- src/main/java/br/com/rodoviaria/spring_clean_arch/infrastructure/adapters/BCryptSenhaEncoderAdapter.java
+++ src/main/java/br/com/rodoviaria/spring_clean_arch/infrastructure/adapters/BCryptSenhaEncoderAdapter.java
@@ -1,4 +1,25 @@
 package br.com.rodoviaria.spring_clean_arch.infrastructure.adapters;
 
-public class BCryptSenhaEncoderAdapter {
+import br.com.rodoviaria.spring_clean_arch.application.ports.out.senha.SenhaEncoderPort;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class BCryptSenhaEncoderAdapter implements SenhaEncoderPort {
+    private final PasswordEncoder encoder;
+    public BCryptSenhaEncoderAdapter(PasswordEncoder encoder) {
+        this.encoder = encoder;
+    }
+
+    // Contrato encode
+    @Override
+    public String encode(String senha){
+        return encoder.encode(senha);
+    }
+
+    // Contrato matches
+    @Override
+    public boolean matches(String senhaBruta, String senhaCodificada){
+        return encoder.matches(senhaBruta, senhaCodificada);
+    }
+
+
 }
--- src/main/java/br/com/rodoviaria/spring_clean_arch/infrastructure/adapters/JwtTokenServiceAdapter.java
+++ src/main/java/br/com/rodoviaria/spring_clean_arch/infrastructure/adapters/JwtTokenServiceAdapter.java
@@ -1,4 +1,34 @@
 package br.com.rodoviaria.spring_clean_arch.infrastructure.adapters;
 
-public class JwtTokenServiceAdapter {
+import br.com.rodoviaria.spring_clean_arch.application.ports.out.senha.TokenServicePort;
+import br.com.rodoviaria.spring_clean_arch.domain.entities.Passageiro;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service // Anotação para o Spring gerenciá-lo como um serviço
+public class JwtTokenServiceAdapter implements TokenServicePort {
+
+    @Value("${api.security.token.secret}") // Pega a chave do application.properties
+    private String secret;
+
+    @Override
+    public String gerarToken(Passageiro passageiro){
+        Algorithm algorithm = Algorithm.HMAC256(secret);
+        return JWT.create()
+                .withIssuer("rodoviaria-api")
+                .withSubject(passageiro.getEmail().getEmail())
+                .withClaim("id", passageiro.getId().toString())
+                .withExpiresAt(getExpirationDate())
+                .sign(algorithm);
+    }
+
+    private Instant getExpirationDate(){
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
 }
