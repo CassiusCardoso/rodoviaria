f1902f6e4080aa81ac0ea18f57d9f921a4fce684
CassiusCardoso <cassiuscardosoo@gmail.com>
2025-07-10 14:39:33 -0300

    (infra-fixed): adapter alterado

--- src/main/java/br/com/rodoviaria/spring_clean_arch/infrastructure/adapters/JwtTokenServiceAdapter.java
+++ src/main/java/br/com/rodoviaria/spring_clean_arch/infrastructure/adapters/JwtTokenServiceAdapter.java
@@ -1,9 +1,11 @@
 package br.com.rodoviaria.spring_clean_arch.infrastructure.adapters;
 
 import br.com.rodoviaria.spring_clean_arch.application.ports.out.senha.TokenServicePort;
+import br.com.rodoviaria.spring_clean_arch.domain.entities.Administrador;
 import br.com.rodoviaria.spring_clean_arch.domain.entities.Passageiro;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -28,6 +30,31 @@ public class JwtTokenServiceAdapter implements TokenServicePort {
                 .sign(algorithm);
     }
 
+    @Override
+    public String gerarToken(Administrador administrador) {
+        Algorithm algorithm = Algorithm.HMAC256(secret);
+        return JWT.create()
+                .withIssuer("rodoviaria-api")
+                .withSubject(administrador.getEmail().getEmail())
+                .withClaim("id", administrador.getId().toString())
+                .withExpiresAt(getExpirationDate())
+                .sign(algorithm);
+    }
+
+    // Validar token
+    public String validarToken(String token){
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            JWT.require(algorithm)
+                    .withIssuer("rodoviaria-api")
+                    .build()
+                    .verify(token);
+            return JWT.decode(token).getSubject(); // Retorna o assunto (email) do token se for válido
+        }catch(JWTVerificationException exception){
+            // Retornar uma string vazia se o token for inválido
+            return "";
+        }
+    }
     private Instant getExpirationDate(){
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
     }
