b107e6de80cdf7e22267ae7e7c968fd3e3aa1ae2
CassiusCardoso <cassiuscardosoo@gmail.com>
2025-07-10 14:39:11 -0300

    (config): alteração nos arquivos de configuração de bean e segurança de endpoints

--- src/main/java/br/com/rodoviaria/spring_clean_arch/infrastructure/config/BeanConfiguration.java
+++ src/main/java/br/com/rodoviaria/spring_clean_arch/infrastructure/config/BeanConfiguration.java
@@ -2,64 +2,235 @@ package br.com.rodoviaria.spring_clean_arch.infrastructure.config;
 
 import br.com.rodoviaria.spring_clean_arch.application.ports.out.senha.SenhaEncoderPort;
 import br.com.rodoviaria.spring_clean_arch.application.ports.out.senha.TokenServicePort;
+import br.com.rodoviaria.spring_clean_arch.application.usecases.admin.AutenticarAdminUseCase;
+import br.com.rodoviaria.spring_clean_arch.application.usecases.linha.*;
+import br.com.rodoviaria.spring_clean_arch.application.usecases.onibus.*;
 import br.com.rodoviaria.spring_clean_arch.application.usecases.passageiro.*;
-import br.com.rodoviaria.spring_clean_arch.domain.repositories.PassageiroRepository;
-import br.com.rodoviaria.spring_clean_arch.domain.repositories.TicketRepository;
-import br.com.rodoviaria.spring_clean_arch.domain.repositories.ViagemRepository;
+import br.com.rodoviaria.spring_clean_arch.application.usecases.ticket.*;
+import br.com.rodoviaria.spring_clean_arch.application.usecases.viagem.*;
+import br.com.rodoviaria.spring_clean_arch.domain.repositories.*;
 import br.com.rodoviaria.spring_clean_arch.infrastructure.adapters.BCryptSenhaEncoderAdapter;
+import br.com.rodoviaria.spring_clean_arch.infrastructure.security.AdminAutenticaoService;
+import br.com.rodoviaria.spring_clean_arch.infrastructure.security.AutenticacaoService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.beans.BeanProperty;
 
 @Configuration
 public class BeanConfiguration {
 
-        // Você REMOVE o bean daqui.
-    /*
+    // == BEANS DE SEGURANÇA ==
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    public AutenticacaoService autenticacaoService(PassageiroRepository passageiroRepository) {
+        return new AutenticacaoService(passageiroRepository);
     }
-    */
 
     @Bean
-    public SenhaEncoderPort senhaEncoderPort(PasswordEncoder encoder){
+    public AdminAutenticaoService adminAutenticaoService(AdministradorRepository administradorRepository) {
+        return new AdminAutenticaoService(administradorRepository);
+    }
+
+    // ADMIN
+    @Bean
+    public AutenticarAdminUseCase autenticarAdminUseCase(
+            AdministradorRepository administradorRepository,
+            SenhaEncoderPort senhaEncoderPort,
+            TokenServicePort tokenServicePort
+    ) {
+        return new AutenticarAdminUseCase(administradorRepository, senhaEncoderPort, tokenServicePort);
+    }
+
+    @Bean
+    public SenhaEncoderPort senhaEncoderPort(PasswordEncoder encoder) {
         return new BCryptSenhaEncoderAdapter(encoder);
     }
+
     @Bean
     public CadastrarPassageiroUseCase cadastrarPassageiroUseCase(
             PassageiroRepository passageiroRepository,
-            SenhaEncoderPort encoder){
+            SenhaEncoderPort encoder) {
         return new CadastrarPassageiroUseCase(passageiroRepository, encoder);
     }
+
     @Bean
     public DesativarPassageiroUseCase desativarPassageiroUseCase(
-            PassageiroRepository passageiroRepository){
+            PassageiroRepository passageiroRepository) {
         return new DesativarPassageiroUseCase(passageiroRepository);
     }
+
     @Bean
     public AtualizarInformacoesPassageiroUseCase atualizarInformacoesPassageiroUseCase(
-            PassageiroRepository passageiroRepository){
+            PassageiroRepository passageiroRepository) {
         return new AtualizarInformacoesPassageiroUseCase(passageiroRepository);
     }
 
     @Bean
     public ListarPassageirosDeUmaViagemUseCase listarPassageirosDeUmaViagemUseCase(
             TicketRepository ticketRepository,
-            ViagemRepository viagemRepository){
+            ViagemRepository viagemRepository) {
         return new ListarPassageirosDeUmaViagemUseCase(ticketRepository, viagemRepository);
     }
+
     @Bean
     public BuscarInformacoesPassageiroUseCase buscarInformacoesPassageiroUseCase(
-            PassageiroRepository passageiroRepository){
+            PassageiroRepository passageiroRepository) {
         return new BuscarInformacoesPassageiroUseCase(passageiroRepository);
     }
+
     @Bean
     public AutenticarPassageiroUseCase autenticarPassageiroUseCase(
             PassageiroRepository passageiroRepository,
             SenhaEncoderPort senhaEncoderPort,
-            TokenServicePort tokenServicePort){
+            TokenServicePort tokenServicePort) {
         return new AutenticarPassageiroUseCase(passageiroRepository, senhaEncoderPort, tokenServicePort);
     }
+
+    // Ônibus
+    @Bean
+    public AtualizarOnibusUseCase atualizarOnibusUseCase(
+            OnibusRepository onibusRepository,
+            ViagemRepository viagemRepository
+    ) {
+        return new AtualizarOnibusUseCase(onibusRepository, viagemRepository);
+    }
+
+    @Bean
+    public BuscarInformacoesOnibusUseCase buscarInformacoesOnibusUseCase(
+            OnibusRepository onibusRepository
+    ) {
+        return new BuscarInformacoesOnibusUseCase(onibusRepository);
+    }
+
+    @Bean
+    public CadastrarOnibusUseCase cadastrarOnibusUseCase(
+            OnibusRepository onibusRepository
+    ) {
+        return new CadastrarOnibusUseCase(onibusRepository);
+    }
+
+    @Bean
+    public DesativarOnibusUseCase desativarOnibusUseCase(
+            OnibusRepository onibusRepository,
+            ViagemRepository viagemRepository
+    ) {
+        return new DesativarOnibusUseCase(onibusRepository, viagemRepository);
+    }
+
+    @Bean
+    public ListarTodosOnibusUseCase listarTodosOnibusUseCase(
+            OnibusRepository onibusRepository
+    ) {
+        return new ListarTodosOnibusUseCase(onibusRepository);
+    }
+
+    // Linha
+    @Bean
+    public BuscarViagensPorRotaUseCase buscarInformacoesDaLinhaUseCase(
+            LinhaRepository linhaRepository,
+            ViagemRepository viagemRepository
+    ) {
+        return new BuscarViagensPorRotaUseCase(linhaRepository, viagemRepository);
+    }
+
+    @Bean
+    public AtualizarInformacoesDaLinhaUseCase atualizarInformacoesDaLinhaUseCase(
+            LinhaRepository linhaRepository) {
+        return new AtualizarInformacoesDaLinhaUseCase(linhaRepository);
+    }
+
+    @Bean
+    public ListarTodasLinhasUseCase listarTodasLinhasUseCase(
+            LinhaRepository linhaRepository
+    ) {
+        return new ListarTodasLinhasUseCase(linhaRepository);
+    }
+
+    @Bean
+    public DesativarLinhaUseCase desativarLinhaUseCase(LinhaRepository linhaRepository) {
+        return new DesativarLinhaUseCase(linhaRepository);
+    }
+
+    @Bean
+    public BuscarLinhaPorOrigemDestinoUseCase buscarLinhaPorOrigemDestinoUseCase(
+            LinhaRepository linhaRepository
+    ) {
+        return new BuscarLinhaPorOrigemDestinoUseCase(linhaRepository);
+    }
+
+    @Bean
+    public CadastrarLinhaUseCase cadastarLinhaUseCase(LinhaRepository linhaRepository) {
+        return new CadastrarLinhaUseCase(linhaRepository);
+    }
+
+    @Bean
+    public BuscarLinhaPorIdUseCase buscarLinhaPorIdUseCase(LinhaRepository linhaRepository) {
+        return new BuscarLinhaPorIdUseCase(linhaRepository);
+    }
+
+    @Bean
+    public AdminCancelarTicketUseCase adminCancelarTicketUseCase(TicketRepository ticketRepository) {
+        return new AdminCancelarTicketUseCase(ticketRepository);
+    }
+
+    @Bean
+    public PassageiroCancelarTicketUseCase passageiroCancelarTicketUseCase(TicketRepository ticketRepository) {
+        return new PassageiroCancelarTicketUseCase(ticketRepository);
+    }
+
+    @Bean
+    public AtualizarTicketUseCase atualizarTicketUseCase(TicketRepository ticketRepository) {
+        return new AtualizarTicketUseCase(ticketRepository);
+    }
+
+    @Bean
+    public ComprarTicketUseCase comprarTicketUseCase(
+            ViagemRepository viagemRepository,
+            TicketRepository ticketRepository,
+            PassageiroRepository passageiroRepository) {
+        return new ComprarTicketUseCase(viagemRepository, ticketRepository, passageiroRepository);
+    }
+
+    @Bean
+    public ListarMeusTicketsUseCase listarMeusTicketsUseCase(TicketRepository ticketRepository) {
+        return new ListarMeusTicketsUseCase(ticketRepository);
+    }
+
+    @Bean
+    public BuscarDetalhesDoTicketUseCase buscarDetalhesDoTicketUseCase(TicketRepository ticketRepository) {
+        return new BuscarDetalhesDoTicketUseCase(ticketRepository);
+    }
+
+    // Viagem
+    @Bean
+    public AtualizarViagemUseCase atualizarViagemUseCase(ViagemRepository viagemRepository, OnibusRepository onibusRepository) {
+        return new AtualizarViagemUseCase(viagemRepository, onibusRepository);
+    }
+
+    @Bean
+    public BuscarViagensDisponiveisUseCase buscarViagensDisponiveisUseCase(ViagemRepository viagemRepository) {
+        return new BuscarViagensDisponiveisUseCase(viagemRepository);
+    }
+
+    @Bean
+    public CancelarViagemUseCase cancelarViagemUseCase(ViagemRepository viagemRepository){
+        return new CancelarViagemUseCase(viagemRepository);
+    }
+
+    @Bean
+    public ListarViagensPorPassageiro listarViagensPorPassageiro(ViagemRepository viagemRepository, PassageiroRepository passageiroRepository){
+        return new ListarViagensPorPassageiro(viagemRepository, passageiroRepository);
+    }
+
+    @Bean
+    public CriarViagemUseCase criarViagemUseCase(ViagemRepository viagemRepository, OnibusRepository onibusRepository, LinhaRepository linhaRepository){
+        return new CriarViagemUseCase(viagemRepository, linhaRepository, onibusRepository);
+    }
+    @Bean
+    public BuscarViagensPorLinhaUseCase buscarViagensPorLinhaUseCase(ViagemRepository viagemRepository, LinhaRepository linhaRepository){
+        return new BuscarViagensPorLinhaUseCase(viagemRepository, linhaRepository);
+    }
 }
+
+
--- src/main/java/br/com/rodoviaria/spring_clean_arch/infrastructure/config/SecurityConfiguration.java
+++ src/main/java/br/com/rodoviaria/spring_clean_arch/infrastructure/config/SecurityConfiguration.java
@@ -1,7 +1,13 @@
 package br.com.rodoviaria.spring_clean_arch.infrastructure.config;
 
+import br.com.rodoviaria.spring_clean_arch.domain.repositories.AdministradorRepository;
+import br.com.rodoviaria.spring_clean_arch.domain.repositories.PassageiroRepository;
+import br.com.rodoviaria.spring_clean_arch.infrastructure.adapters.JwtTokenServiceAdapter;
+import br.com.rodoviaria.spring_clean_arch.infrastructure.security.AdminSecurityFilter;
+import br.com.rodoviaria.spring_clean_arch.infrastructure.security.SecurityFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -11,25 +17,57 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+
+
+    // --- Beans de Filtro ---
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
-        return httpSecurity
-                // Desativa a proteção CSRF, comum em APIs stateless (sem sessão)
+    public SecurityFilter securityFilter(JwtTokenServiceAdapter tokenService, PassageiroRepository repository) {
+        return new SecurityFilter(tokenService, repository);
+    }
+
+    @Bean
+    public AdminSecurityFilter adminSecurityFilter(JwtTokenServiceAdapter tokenService, AdministradorRepository repository) {
+        return new AdminSecurityFilter(tokenService, repository);
+    }
+
+    // --- Cadeias de Segurança ---
+
+    @Bean
+    @Order(1) // Prioridade 1: Regras do Admin são verificadas primeiro
+    public SecurityFilterChain adminFilterChain(HttpSecurity http, AdminSecurityFilter adminSecurityFilter) throws Exception {
+        return http
+                .securityMatcher("/admin/**") // Aplica este filtro APENAS para rotas /admin/**
                 .csrf(csrf -> csrf.disable())
-                // Configura a política de sessão para ser stateless
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                // Configura as regras de autorização para cada endpoint
                 .authorizeHttpRequests(authorize -> authorize
-                        // Permite acesso público aos endpoints de login e cadastro
+                        .requestMatchers(HttpMethod.POST, "/admin/login").permitAll() // Permite login de admin
+                        .anyRequest().hasRole("ADMIN") // Qualquer outra rota /admin/** exige a role ADMIN
+                )
+                .addFilterBefore(adminSecurityFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    @Order(2) // Prioridade 2: Regras para o resto da aplicação
+    public SecurityFilterChain appFilterChain(HttpSecurity http, SecurityFilter securityFilter) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        // Endpoints públicos
                         .requestMatchers(HttpMethod.POST, "/passageiros/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/passageiros").permitAll()
-                        // Para qualquer outra requisição, o usuário precisa estar autenticado
+                        .requestMatchers(HttpMethod.GET, "/linhas/**").permitAll() // Consultas de linhas/viagens são públicas
+
+                        // Qualquer outra requisição precisa de autenticação (de passageiro)
                         .anyRequest().authenticated()
                 )
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
