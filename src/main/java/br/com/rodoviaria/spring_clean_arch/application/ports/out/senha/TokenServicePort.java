fe5d64f2d9a3411c7696008a1a73210d34b849d3
CassiusCardoso <cassiuscardosoo@gmail.com>
2025-07-10 14:42:11 -0300

    (fixed): contrato gerarToken(Administrador administrador);

--- src/main/java/br/com/rodoviaria/spring_clean_arch/application/ports/out/senha/TokenServicePort.java
+++ src/main/java/br/com/rodoviaria/spring_clean_arch/application/ports/out/senha/TokenServicePort.java
@@ -1,7 +1,10 @@
 package br.com.rodoviaria.spring_clean_arch.application.ports.out.senha;
 
+import br.com.rodoviaria.spring_clean_arch.domain.entities.Administrador;
 import br.com.rodoviaria.spring_clean_arch.domain.entities.Passageiro;
 
 public interface TokenServicePort {
     String gerarToken(Passageiro passageiro);
+    String gerarToken(Administrador administrador);
+
 }
