d5fa7d4fb306ad9f28a6eedd2e34dceb226b5ec2
CassiusCardoso <cassiuscardosoo@gmail.com>
2025-07-11 14:41:14 -0300

    (Junit e Mockito): testes unitários

--- src/test/java/br/com/rodoviaria/spring_clean_arch/application/usecases/passageiro/AtualizarInformacoesPassageiroUseCaseTest.java
+++ src/test/java/br/com/rodoviaria/spring_clean_arch/application/usecases/passageiro/AtualizarInformacoesPassageiroUseCaseTest.java
@@ -3,6 +3,8 @@ package br.com.rodoviaria.spring_clean_arch.application.usecases.passageiro;
 import br.com.rodoviaria.spring_clean_arch.application.dto.request.passageiro.AtualizarInformacoesPassageiroRequest;
 import br.com.rodoviaria.spring_clean_arch.application.dto.response.passageiro.AtualizarInformacoesPassageiroResponse;
 import br.com.rodoviaria.spring_clean_arch.domain.entities.Passageiro;
+import br.com.rodoviaria.spring_clean_arch.domain.exceptions.passageiro.NomeInvalidoException;
+import br.com.rodoviaria.spring_clean_arch.domain.exceptions.passageiro.PassageiroInvalidoException;
 import br.com.rodoviaria.spring_clean_arch.domain.repositories.PassageiroRepository;
 import br.com.rodoviaria.spring_clean_arch.domain.valueobjects.Cpf;
 import br.com.rodoviaria.spring_clean_arch.domain.valueobjects.Email;
@@ -20,8 +22,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -56,7 +57,7 @@ public class AtualizarInformacoesPassageiroUseCaseTest {
 
         // ARRANGE
         AtualizarInformacoesPassageiroRequest request = new AtualizarInformacoesPassageiroRequest(
-                "John Doe novo",
+                "John Doe Novo",
                 "john.doe.novo@gmail.com",
                 "Senha123@",
                 "129.425.841-90",
@@ -64,7 +65,7 @@ public class AtualizarInformacoesPassageiroUseCaseTest {
         );
         when(repository.buscarPassageiroPorId(passageiroId)).thenReturn(Optional.of(passageiro));
 
-        // Quando o método 'salvar' for chamado, retornar o passageiro com atualizações
+        // Quando o método 'salvar' for chamado, retornar o passageiro atualizado
         when(repository.salvar(any(Passageiro.class))).thenAnswer(invocation -> invocation.getArgument(0));
 
         // ACT
@@ -72,7 +73,7 @@ public class AtualizarInformacoesPassageiroUseCaseTest {
 
         // ASSERT
         assertNotNull(response);
-        assertEquals("John doe novo", response.nome());
+        assertEquals("John Doe Novo", response.nome());
         assertEquals("john.doe.novo@gmail.com", response.email());
 
         ArgumentCaptor<Passageiro> passageiroCaptor = ArgumentCaptor.forClass(Passageiro.class);
@@ -83,7 +84,156 @@ public class AtualizarInformacoesPassageiroUseCaseTest {
         Passageiro passageiroSalvo = passageiroCaptor.getValue();
         assertEquals("John Doe Novo", passageiroSalvo.getNome());
         assertEquals("john.doe.novo@gmail.com", passageiroSalvo.getEmail().getEmail());
-        assertEquals("55566677788", passageiroSalvo.getCpf().getCpf()); // Comparando com o CPF limpo
+
+        // Compare o valor limpo do CPF do request com o valor do objeto salvo.
+        String cpfLimpo = request.cpf().replaceAll("[^\\d]", "");
+        assertEquals(cpfLimpo, passageiroSalvo.getCpf().getCpf());
+    }
+
+    @Test
+    @DisplayName("Deve lançar exceção quando o passageiro for inativo")
+    void deveLancarExcecao_QuandoPassageiroForInativo(){
+        // ARRANGE
+        // CRIAR UMA INSTÂNCIA DE UM PASSAGEIRO INATIVO
+        // PODE SER O PASSAGEIRO DO setUp()
+        Passageiro passageiroInativo = passageiro.desativar();
+
+        // CRIAR UM REQUEST DE ATUALIZAÇÃO QUE SEJA COM QUALQUER CONTEÚDO
+        // A EXECUÇÃO TEM QUE FALHAR ANTES DE OS DADOS SEREM USADOS
+        AtualizarInformacoesPassageiroRequest request = new AtualizarInformacoesPassageiroRequest(
+                "Nome Novo", "email.novo@gmail.com", "NovaSenha@123", "987.654.321-00", "(33) 94444-5555"
+        );
+        // QUANDO O REPOSITÓRIO BUSCAR O PASSAGEIRO POR ID, FINGIR QUE ENCONTROU O PASSAGEIRO INATIVO
+
+        when(repository.buscarPassageiroPorId(passageiroId)).thenReturn(Optional.of(passageiroInativo));
+
+        // ACT e ASSERT
+        // Verificando se a exceção encontrada é a correta (PassageiroInvalidoException)
+        assertThrows(PassageiroInvalidoException.class, () -> {
+            atualizarInformacoesPassageiroUseCase.execute(request, passageiroId);
+        }, "Deveria ter lançado PassageiroInvalidoException, mas não o fez");
+
+        // VERIFY
+        // GARANTIR QUE O MÉTODO salvar nunca foi chamado
+        // PORQUE A LÓGICA DEVE PARAR no status ativo
+        verify(repository, never()).salvar(any(Passageiro.class));
+    }
+
+    @Test
+    @DisplayName("Deve lançar exceção quando o passageiro informar um email inválido para atualizar")
+    void deveLancarExcecao_QuandoEmailForInvalido(){
+
+        // ARRANGE
+        Passageiro passageiroInvalido = passageiro;
+
+        AtualizarInformacoesPassageiroRequest request = new AtualizarInformacoesPassageiroRequest(
+                "John", "Jonh@yahoo.com","123456@", "129.425.841-90", "(11) 96589-1605"
+        );
+
+        when(repository.buscarPassageiroPorId(passageiroId)).thenReturn(Optional.of(passageiroInvalido));
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            atualizarInformacoesPassageiroUseCase.execute(request, passageiroId);
+        }, "Deveria ter lançado PassageiroInvalidoException, mas não o fez");
+
+        verify(repository, never()).salvar(any(Passageiro.class));
 
     }
+
+    @Test
+    @DisplayName("Deve lançar exceção quando um passageiro não for encontrado")
+    void deveLancarExcecao_QuandoPassageiroNaoForEncontrado(){
+        // ARRANGE
+        Passageiro passageiroNaoEncontrado = null;
+
+        when(repository.buscarPassageiroPorId(passageiroId)).thenReturn(Optional.ofNullable(passageiroNaoEncontrado));
+
+        AtualizarInformacoesPassageiroRequest request =  new AtualizarInformacoesPassageiroRequest("a", "l", "a", "2", "1");
+
+        assertThrows(PassageiroInvalidoException.class, () -> {
+            atualizarInformacoesPassageiroUseCase.execute(request, passageiroId);
+        }, "Deveria ter lançado exceção, mas não o fez");
+
+        verify(repository, never()).salvar(any(Passageiro.class));
+    }
+
+    @Test
+    @DisplayName("Deve lançar uma exceção quando o CPF for inválido")
+    void deveLancarExcecao_QuandoCpfForInvalido(){
+
+        // ARRANGE
+        Passageiro passageiroInvalido = passageiro;
+
+        AtualizarInformacoesPassageiroRequest request = new AtualizarInformacoesPassageiroRequest(
+                "John Travolta", "johntravolta@gmail.com", "123456@", "123.456-789.10", "(11) 96589-1605"
+        );
+
+        when(repository.buscarPassageiroPorId(passageiroId)).thenReturn(Optional.of(passageiroInvalido));
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            atualizarInformacoesPassageiroUseCase.execute(request, passageiroId);
+        }, "Deveria ter lançado uma exceção mas não lançou");
+
+        verify(repository, never()).salvar(any(Passageiro.class));
+
+    }
+
+    @Test
+    @DisplayName("Deve lançar uma exceção quando a senha for inválida.")
+    void deveLancarExcecao_QuandoSenhaForInvalida(){
+        // ARRANGE
+        Passageiro passageiroInvalido = passageiro;
+
+        AtualizarInformacoesPassageiroRequest request = new AtualizarInformacoesPassageiroRequest(
+                "Jols", "jols1@gmail.com", "12345", "129.425.841-90", "(11) 96589-1605"
+        );
+        when(repository.buscarPassageiroPorId(passageiroId)).thenReturn(Optional.of(passageiroInvalido));
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            atualizarInformacoesPassageiroUseCase.execute(request, passageiroId);
+        }, "Deveria ter lançado uma exceção mas não lançou.");
+
+        verify(repository, never()).salvar(any(Passageiro.class));
+    }
+
+    @Test
+    @DisplayName("Deve lançar uma exceção quando o número do telefone for inválido.")
+    void deveLancarExcecao_QuandoTelefoneForInvalido(){
+        // ARRANGE
+        Passageiro passageiroInvalido = passageiro;
+
+        AtualizarInformacoesPassageiroRequest request = new AtualizarInformacoesPassageiroRequest(
+                "jan", "jan@gmail.com", "123456@", "129.425.841-90", "11 96589-16025"
+        );
+
+        when(repository.buscarPassageiroPorId(passageiroId)).thenReturn(Optional.of(passageiroInvalido));
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            atualizarInformacoesPassageiroUseCase.execute(request, passageiroId);
+        }, "Deve lançar uma exceção mas não lançou");
+
+
+        verify(repository, never()).salvar(any(Passageiro.class));
+    }
+
+    @Test
+    @DisplayName("Deve lançar uma exceção quando o nome for vazio.")
+    void deveLancarExcecao_QuandoNomeForVazio(){
+        // ARRANGE
+        Passageiro passageiroInvalido = passageiro;
+
+        AtualizarInformacoesPassageiroRequest request = new AtualizarInformacoesPassageiroRequest(
+                "", "oller@gmail.com", "12345@g0","129.425.841-90", "(11) 96589-1605"
+        );
+
+        when(repository.buscarPassageiroPorId(passageiroId)).thenReturn(Optional.of(passageiroInvalido));
+
+        assertThrows(NomeInvalidoException.class, () ->{
+            atualizarInformacoesPassageiroUseCase.execute(request, passageiroId);
+        }, "Deve lançar uma exceção, mas não fez.");
+
+        verify(repository, never()).salvar(any(Passageiro.class));
+    }
+
+
 }
