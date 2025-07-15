05857584542cd3e3e091c85917982319b2f026a7
CassiusCardoso <cassiuscardosoo@gmail.com>
2025-07-15 09:21:41 -0300

    (refactor): refatorado

--- src/main/java/br/com/rodoviaria/spring_clean_arch/infrastructure/adapters/JwtTokenServiceAdapter.java
+++ src/main/java/br/com/rodoviaria/spring_clean_arch/infrastructure/adapters/JwtTokenServiceAdapter.java
@@ -6,6 +6,8 @@ import br.com.rodoviaria.spring_clean_arch.domain.entities.Passageiro;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -13,14 +15,20 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 
-@Service // Anotação para o Spring gerenciá-lo como um serviço
+@Service
 public class JwtTokenServiceAdapter implements TokenServicePort {
 
-    @Value("${api.security.token.secret}") // Pega a chave do application.properties.old
+    private static final Logger logger = LoggerFactory.getLogger(JwtTokenServiceAdapter.class);
+
+    @Value("${api.security.token.secret}")
     private String secret;
 
+    @Value("${app.debug.profile}")
+    private String debugProfile;
+
     @Override
-    public String gerarToken(Passageiro passageiro){
+    public String gerarToken(Passageiro passageiro) {
+        logger.info("Profile em uso: {}", debugProfile);
         Algorithm algorithm = Algorithm.HMAC256(secret);
         return JWT.create()
                 .withIssuer("rodoviaria-api")
@@ -32,6 +40,7 @@ public class JwtTokenServiceAdapter implements TokenServicePort {
 
     @Override
     public String gerarToken(Administrador administrador) {
+        logger.info("Profile em uso: {}", debugProfile);
         Algorithm algorithm = Algorithm.HMAC256(secret);
         return JWT.create()
                 .withIssuer("rodoviaria-api")
@@ -41,21 +50,21 @@ public class JwtTokenServiceAdapter implements TokenServicePort {
                 .sign(algorithm);
     }
 
-    // Validar token
-    public String validarToken(String token){
-        try{
+    public String validarToken(String token) {
+        try {
+            logger.info("Profile em uso: {}", debugProfile);
             Algorithm algorithm = Algorithm.HMAC256(secret);
             JWT.require(algorithm)
                     .withIssuer("rodoviaria-api")
                     .build()
                     .verify(token);
-            return JWT.decode(token).getSubject(); // Retorna o assunto (email) do token se for válido
-        }catch(JWTVerificationException exception){
-            // Retornar uma string vazia se o token for inválido
+            return JWT.decode(token).getSubject();
+        } catch (JWTVerificationException exception) {
             return "";
         }
     }
-    private Instant getExpirationDate(){
+
+    private Instant getExpirationDate() {
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
     }
 }
