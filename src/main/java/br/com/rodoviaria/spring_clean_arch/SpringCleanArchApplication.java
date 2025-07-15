b5d8914700c25167ad983a3a90cedb35a3ab55c6
CassiusCardoso <cassiuscardosoo@gmail.com>
2025-07-15 09:21:15 -0300

    (app): documentação swagger adicionada

--- src/main/java/br/com/rodoviaria/spring_clean_arch/SpringCleanArchApplication.java
+++ src/main/java/br/com/rodoviaria/spring_clean_arch/SpringCleanArchApplication.java
@@ -1,9 +1,20 @@
 package br.com.rodoviaria.spring_clean_arch;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// Para que o Swagger UI mostre um campo "Authorize" onde você pode colar o token JWT, adicione uma configuração na sua classe principal.
+@OpenAPIDefinition(info = @Info(title = "Rodoviária API", version = "v1", description = "Documentação da API do sistema de rodoviária"))
+@SecurityScheme(
+		name = "bearerAuth",
+		type = SecuritySchemeType.HTTP,
+		bearerFormat = "JWT",
+		scheme = "bearer")
 public class
 SpringCleanArchApplication {
 
