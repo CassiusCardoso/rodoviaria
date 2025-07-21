108d28430ba518ed394bea829da4384cccab0152
CassiusCardoso <cassiuscardosoo@gmail.com>
2025-07-21 08:29:41 -0300

    (refactor): serviço de e-mail refatorado

--- src/main/java/br/com/rodoviaria/spring_clean_arch/application/ports/out/email/EmailServicePort.java
+++ src/main/java/br/com/rodoviaria/spring_clean_arch/application/ports/out/email/EmailServicePort.java
@@ -3,4 +3,7 @@ package br.com.rodoviaria.spring_clean_arch.application.ports.out.email;
 // Este é o "contrato" que o Caso de Uso irá conhecer.
 public interface EmailServicePort {
     void enviarEmail(String destinatario, String assunto, String corpo);
+
+    void enviarComAnexo(String titulo, String mensagem, String destinatario, byte[] anexo, String nomeAnexo);
+
 }
